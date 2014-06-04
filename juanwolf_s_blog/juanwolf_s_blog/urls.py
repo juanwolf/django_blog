dbd86c1fe8b9c0f82a546cfb14bf39321daf95ac
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-06-04 14:00:59 +0200

    Django project initialisation

---
+++ juanwolf_s_blog/juanwolf_s_blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'juanwolf_s_blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
