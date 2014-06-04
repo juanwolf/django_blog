d0b032ab0cd0ebcf3f211c0d5adf6d681abaf198
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-06-04 21:26:55 +0200

    Added administrators tests

--- juanwolf_s_blog/blogengine/tests.py
+++ juanwolf_s_blog/blogengine/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, LiveServerTestCase, Client
 from django.utils import timezone
 from blogengine.models import Post
 
@@ -27,27 +28,60 @@ class PostTest(TestCase):
 
         # Check we can find it
         all_posts = Post.objects.all()
-        self.assertEquals(len(all_posts), 1)
+        self.assertEqual(len(all_posts), 1)
         only_post = all_posts[0]
-        self.assertEquals(only_post, post)
+        self.assertEqual(only_post, post)
 
         # Check attributes
-        self.assertEquals(only_post.title, 'My first post')
-        self.assertEquals(only_post.text, 'This is my first post that is so awesome')
-        self.assertEquals(only_post.pub_date.day, post.pub_date.day)
-        self.assertEquals(only_post.pub_date.month, post.pub_date.month)
-        self.assertEquals(only_post.pub_date.year, post.pub_date.year)
-        self.assertEquals(only_post.pub_date.hour, post.pub_date.hour)
-        self.assertEquals(only_post.pub_date.minute, post.pub_date.minute)
-        self.assertEquals(only_post.pub_date.second, post.pub_date.second)
+        self.assertEqual(only_post.title, 'My first post')
+        self.assertEqual(only_post.text, 'This is my first post that is so awesome')
+        self.assertEqual(only_post.pub_date.day, post.pub_date.day)
+        self.assertEqual(only_post.pub_date.month, post.pub_date.month)
+        self.assertEqual(only_post.pub_date.year, post.pub_date.year)
+        self.assertEqual(only_post.pub_date.hour, post.pub_date.hour)
+        self.assertEqual(only_post.pub_date.minute, post.pub_date.minute)
+        self.assertEqual(only_post.pub_date.second, post.pub_date.second)
 
 class AdminTest(LiveServerTestCase):
-    def test_login(self):
-        # Create client
-        c = Client()
+    def setUp(self):
+        self.client = Client()
+        self.username = 'Jack'
+        self.email = 'test@test.com'
+        self.password = 'adit'
+        self.test_user = User.objects.create_superuser(self.username, self.email, self.password)
 
+    def test_page_admin_exists(self):
         # Get login page
-        response = c.get('/admin/')
+        response = self.client.get('/admin/')
 
         # Check response code
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(bytes('Log in', 'utf-8') in response.content)
+
+    def test_login(self):
+        login = self.client.login(username='Jack', password='adit')
+        self.assertTrue(login)
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(bytes('Log out', 'utf-8') in response.content)
+
+    def test_logout(self):
+        # Log in
+        self.client.login(username='Jack', password="adit")
+
+        # Check response code
+        response = self.client.get('/admin/')
         self.assertEquals(response.status_code, 200)
+
+        # Check 'Log out' in response
+        self.assertTrue(bytes('Log out', 'utf-8') in response.content)
+
+        # Log out
+        self.client.logout()
+
+        # Check response code
+        response = self.client.get('/admin/')
+        self.assertEquals(response.status_code, 200)
+
+        # Check 'Log in' in response
+        self.assertTrue(bytes('Log in', 'utf-8') in response.content)
