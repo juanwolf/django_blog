44467a9fa0ad23504f335ac9697bc21d94398849
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-06-04 19:14:00 +0200

    Added new default values to the Post model

--- juanwolf_s_blog/blogengine/models.py
+++ juanwolf_s_blog/blogengine/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, default="")
+    pub_date = models.DateTimeField(default=datetime.now)
+    text = models.TextField(default="")
