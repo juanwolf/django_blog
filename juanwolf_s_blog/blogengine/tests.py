8ed20f001d1816023321e3f537be8992720c0e44
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-06-04 18:29:19 +0200

    Added test unit to the Post Model.

--- juanwolf_s_blog/blogengine/tests.py
+++ juanwolf_s_blog/blogengine/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.utils import timezone
+from blogengine.models import Post
 
-# Create your tests here.
+
+class PostTest(TestCase):
+
+    def test_create_empty_post(self):
+        # create a new empty post
+        post = Post()
+        post.save()
+        all_posts = Post.objects.all()
+        first_post = all_posts[0]
+        self.assertEqual(post, first_post)
+
+    def test_create_post(self):
+        # Create the post
+        post = Post()
+
+        # Set the attributes
+        post.title = 'My first post'
+        post.text = 'This is my first post that is so awesome'
+        post.pub_date = timezone.now()
+
+        # Save it
+        post.save()
+
+        # Check we can find it
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 1)
+        only_post = all_posts[0]
+        self.assertEquals(only_post, post)
+
+        # Check attributes
+        self.assertEquals(only_post.title, 'My first post')
+        self.assertEquals(only_post.text, 'This is my first post that is so awesome')
+        self.assertEquals(only_post.pub_date.day, post.pub_date.day)
+        self.assertEquals(only_post.pub_date.month, post.pub_date.month)
+        self.assertEquals(only_post.pub_date.year, post.pub_date.year)
+        self.assertEquals(only_post.pub_date.hour, post.pub_date.hour)
+        self.assertEquals(only_post.pub_date.minute, post.pub_date.minute)
+        self.assertEquals(only_post.pub_date.second, post.pub_date.second)
