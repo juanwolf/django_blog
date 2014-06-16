b519f1aefbb217f9dfdc8e29a6e35eaa3986554b
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-06-16 15:39:57 +0200

    Improved Admin section (title_en -> slug_en, etc..)

--- juanwolf_s_blog/blogengine/admin.py
+++ juanwolf_s_blog/blogengine/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from blogengine.models import Post, Category, Tag
 from django_summernote.admin import SummernoteModelAdmin
 
@@ -6,9 +7,21 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {"slug": ("title",),
                            "slug_en": ("title_en",),
-                           "slug_fr": ("title_fr",),}
+                           "slug_fr": ("title_fr",)}
+
+
+class CategoryAdmin(ModelAdmin):
+    prepopulated_fields = {"slug": ("name",),
+                           "slug_en": ("name_en",),
+                           "slug_fr": ("name_fr",)}
+
+
+class TagAdmin(ModelAdmin):
+    prepopulated_fields = {"slug": ("name",),
+                           "slug_en": ("name_en",),
+                           "slug_fr": ("name_fr",)}
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Category)
-admin.site.register(Tag)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
