de8f4cb5714b32d088e712943b63897437ae2ccd
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-06-16 21:10:39 +0200

    Added 404 handler to productions environment

--- juanwolf_s_blog/juanwolf_s_blog/urls.py
+++ juanwolf_s_blog/juanwolf_s_blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from blogengine.views import PageNotFoundView
 from juanwolf_s_blog import settings
 
 admin.autodiscover()
@@ -9,3 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blogengine.urls')),
 )
+
+handler404 = PageNotFoundView.as_view()
