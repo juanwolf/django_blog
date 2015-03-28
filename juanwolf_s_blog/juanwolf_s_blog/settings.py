b2dcb24d6ac14b207403901ffde7f4994e6f40b7
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2015-03-28 22:36:17 +0100

    fix(): Adding again the security for clickjacking.

--- juanwolf_s_blog/juanwolf_s_blog/settings.py
+++ juanwolf_s_blog/juanwolf_s_blog/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
     'django.middleware.locale.LocaleMiddleware',
 )
 
