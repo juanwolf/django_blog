75feead1c4bf73b267f0f678d1b6f16153ad1880
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2016-01-31 21:34:06 +0100

    feat(api): Adding api for posts.

--- juanwolf_s_blog/api/views.py
+++ juanwolf_s_blog/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api import serializers
+from blogengine import models
+
+
+class PostViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    A simple viewset to retrieve all the posts of blog.juanwolf.fr.
+    """
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
--- juanwolf_s_blog/juanwolf_s_blog/urls.py
+++ juanwolf_s_blog/juanwolf_s_blog/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include('api.urls')),
     url(r'', include('blogengine.urls')),
 )
 
