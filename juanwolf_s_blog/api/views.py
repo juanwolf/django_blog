99b787d8ec744500314c15d0d500d5c6768fbbc2
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2016-02-02 22:37:33 +0100

    fix(api): Decreasing SQL request number. ,

--- juanwolf_s_blog/api/views.py
+++ juanwolf_s_blog/api/views.py
@@ -8,5 +8,5 @@ class PostViewSet(viewsets.ReadOnlyModelViewSet):
     """
     A simple viewset to retrieve all the posts of blog.juanwolf.fr.
     """
-    queryset = models.Post.objects.all()
+    queryset = models.Post.objects.all().select_related('category').prefetch_related('tags')
     serializer_class = serializers.PostSerializer
