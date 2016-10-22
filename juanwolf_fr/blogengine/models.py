3fe3ab6db2606ece38d0e08540da443b85606182
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2016-10-22 23:24:42 +0100

    Renaming juanwolfs-blog to juanwolf.fr

--- juanwolf_fr/blogengine/models.py
+++ juanwolf_fr/blogengine/models.py
@@ -6,7 +6,7 @@ from django.db import models
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 
-from juanwolfs_blog.settings import MEDIA_URL
+from juanwolf_fr.settings import MEDIA_URL
 
 
 def upload_path(self, filename):
