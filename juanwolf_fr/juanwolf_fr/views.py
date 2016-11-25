b1af50475023797008ea2f11c87f97d1e2c09489
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2016-11-25 22:53:16 +0000

    feat(#8): Calculating age in index view.
    Closes #8

--- juanwolf_fr/juanwolf_fr/views.py
+++ juanwolf_fr/juanwolf_fr/views.py
@@ -1,5 +1,31 @@
+from datetime import date
+
 from django.views import generic
+from django.conf import settings
 
 
-class Index(generic.TemplateView):
+class BirthdayContextMixin(generic.base.ContextMixin):
+    """
+    Add into the context the age of the owner of the website.
+    To change this value please have a look at the BIRTHDAY_DATE setting.
+    """
+
+    def get_age(self):
+        today = date.today()
+        years_delta = today.year - settings.BIRTHDAY_DATE.year
+        is_before_birthday = (today.month, today.day) < (
+            settings.BIRTHDAY_DATE.month, settings.BIRTHDAY_DATE.day
+        )
+        return years_delta - int(is_before_birthday)
+
+    def get_context_data(self, **kwargs):
+        context = super(BirthdayContextMixin, self).get_context_data(**kwargs)
+        context['age'] = self.get_age()
+        return context
+
+
+class IndexView(generic.TemplateView, BirthdayContextMixin):
+    """
+    First page of the website. "The famous 'Index'"
+    """
     template_name = "index.html"
