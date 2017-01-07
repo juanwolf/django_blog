4f0c8d3d29100745448b35abf1a40571e4d129ca
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2017-01-07 12:51:34 +0000

    Remove static files to separate project.

--- juanwolf_fr/resume/views.py
+++
@@ -1,7 +0,0 @@
-from django.views import generic
-
-from juanwolf_fr import mixins
-
-
-class ResumeTemplateView(generic.TemplateView, mixins.BirthdayContextMixin):
-    template_name = "resume.html"
